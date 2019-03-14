ff029a202059e3007e9f47d3c47f1eb0265c555b
James Fairhurst <info@jamesfairhurst.co.uk>
2019-03-14 12:44:24 +0000

    Add config file comments

--- config/pageviews.php
+++ config/pageviews.php
@@ -1,7 +1,10 @@
 <?php
 
 return [
+    // Are pageviews tracked?
     'enabled' => env('PAGEVIEWS_ENABLED', true),
+    // Site's tracking id
     'google_analytics_tracking_id' => env('PAGEVIEWS_GOOGLE_ANALYTICS_TRACKING_ID', ''),
+    // Route name used internally to send pageview to google
     'route' => 'pageviews.store',
 ];
